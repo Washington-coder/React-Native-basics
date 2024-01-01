493abe9bc625d9f9f022f15d9357586d597b1f97
Washington Antonio <was.antonio.xy@gmail.com>
2024-01-01 15:48:36 -0400

    Add TextInput

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 import { styles } from './styles';
 
 export default function Home() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do evento
-      </Text>
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.eventName}>
+                Nome do evento
+            </Text>
+            <Text style={styles.eventDate}>
+                Sexta, 4 de Novembro de 2022.
+            </Text>
+            <TextInput
+                style={styles.input}
+                placeholder='Nome do participante'
+                placeholderTextColor="#6b6b6b"
+            />
+        </View>
+    );
 }
--- imhere/src/screens/Home/styles.ts
+++ imhere/src/screens/Home/styles.ts
@@ -7,13 +7,21 @@ export const styles = StyleSheet.create({
         padding: 24
     },
     eventName: {
-        color: '#fdfcfe',
+        color: '#fff',
         fontSize: 24,
         fontWeight: 'bold',
         marginTop: 48
     },
     eventDate: {
         color: '#6b6b6b',
+        fontSize: 16
+    },
+    input: {
+        height: 46,
+        backgroundColor: '#1f1e25',
+        borderRadius: 5,
+        color: '#fff',
+        padding: 8,
         fontSize: 16
     }
 });
