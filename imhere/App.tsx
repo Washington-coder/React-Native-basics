45011fb718eaec39d48587e29ec016bbe7ad1bb4
Washington Antonio <was.antonio.xy@gmail.com>
2024-01-01 14:58:28 -0400

    StyleSheet styling

--- imhere/App.tsx
+++ imhere/App.tsx
@@ -1,26 +1,7 @@
-import { Text, View } from 'react-native';
+import Home from './src/screens/Home';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24
-    }}>
-      <Text style={{
-        color: '#fdfcfe',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>
-        Nome do evento
-      </Text>
-      <Text style={{
-        color: '#6b6b6b',
-        fontSize: 16
-      }}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
-    </View>
+    <Home/>
   );
 }
